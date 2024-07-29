cbabe0f46fc44fd0e2ec2a632153b4206a5bbbcb
kimixz <kimiakhakzad8@gmail.com>
2024-07-28 21:36:13 -0400

    Fix styling

--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
         <h1 className="font-medium text-2xl mb-8 tracking-tighter">My Publications</h1>
         
         <div className="space-y-4">
-          <div className="p-4 border border-gray-300 rounded-lg">
+          <div className="p-4 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-900 shadow-md">
             <h2 className="font-semibold">
               <a  href="https://dl.acm.org/doi/abs/10.1145/3650105.3652291" target="_blank" rel="noopener noreferrer" className=" hover:underline">
                 Assessing the Impact of GPT-4 Turbo in Generating Defeaters for Assurance Cases
@@ -12,7 +12,7 @@ export default function Page() {
             </h2>
             <p className="text-gray-500">Proceedings of the 2024 IEEE/ACM First International Conference on AI</p>
           </div>
-          <div className="p-4 border border-gray-300 rounded-lg">
+          <div className="p-4 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-900 shadow-md">
             <h2 className="font-semibold">
               <a href="https://arxiv.org/abs/2401.16633" target="_blank" rel="noopener noreferrer" className=" hover:underline">
                 I came, I saw, I certified: some perspectives on the safety assurance of cyber-physical systems
@@ -20,7 +20,7 @@ export default function Page() {
             </h2>
             <p className="text-gray-500">arXiv preprint arXiv:2401.16633</p>
           </div>
-          <div className="p-4 border border-gray-300 rounded-lg">
+          <div className="p-4 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-900 shadow-md">
             <h2 className="font-semibold">
               <a href="https://arxiv.org/abs/2312.05696" target="_blank" rel="noopener noreferrer" className=" hover:underline">
                 GPT-4 and Safety Case Generation: An Exploratory Analysis
@@ -28,7 +28,7 @@ export default function Page() {
             </h2>
             <p className="text-gray-500">arXiv preprint arXiv:2312.05696</p>
           </div>
-          <div className="p-4 border border-gray-300 rounded-lg">
+          <div className="p-4 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-900 shadow-md">
             <h2 className="font-semibold">
               <a href="https://www.sciencedirect.com/science/article/pii/S0950584924001319" target="_blank" rel="noopener noreferrer" className=" hover:underline">
                 A PRISMA-driven systematic mapping study on system assurance weakeners
@@ -36,7 +36,7 @@ export default function Page() {
             </h2>
             <p className="text-gray-500">Information and Software Technology, 107526</p>
           </div>
-          <div className="p-4 border border-gray-300 rounded-lg">
+          <div className="p-4 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-900 shadow-md">
             <h2 className="font-semibold">
               <a href="https://www.sciencedirect.com/science/article/pii/S0957417424015203" target="_blank" rel="noopener noreferrer" className=" hover:underline">
                 Prompting GPT–4 to support automatic safety case generation
